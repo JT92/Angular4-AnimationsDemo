56adc6e9d3832b74899267b950cb4480cb8c63c2
Jack Torres <jack.torres@matrixcare.com>
2017-10-23 16:45:51 -0400

    193 - Use Bidirectional Transition

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -10,11 +10,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 
       state('void', style({ opacity: 0 })),
       
-      transition('void => *', [
-        animate(2000)
-      ]),
-
-      transition('* => void', [
+      transition('void <=> *', [
         animate(2000)
       ])
     ])
