5243d3bf262b460af7f5955923269cc21204c5c7
Jack Torres <jack.torres@matrixcare.com>
2017-10-23 16:46:17 -0400

    193 - Use Alias Transition

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -10,7 +10,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 
       state('void', style({ opacity: 0 })),
       
-      transition('void <=> *', [
+      transition(':enter, :leave', [
         animate(2000)
       ])
     ])
