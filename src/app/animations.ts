0ca0cbfadaf3cb9b8393865790900606bc83bab7
Jack Torres <jack.torres@matrixcare.com>
2017-10-23 16:58:44 -0400

    Assignment 10 - Build reusable slide animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,10 +1,19 @@
 import { trigger, state, transition, style, animate } from '@angular/animations';
 
 export let fade = trigger('fade', [
-    
-        state('void', style({ opacity: 0 })),
-        
+    state('void', style({ opacity: 0 })),       
         transition(':enter, :leave', [
         animate(2000)
-        ])
     ])
+]);
+
+export let slide = trigger('slide', [
+state('void', style({ transform: 'translateX(20px)' })),       
+    transition(':enter', [
+        style({ transform: 'translateX(-10px)' }),
+        animate(500)
+    ]), 
+    transition(':leave', [
+        animate(500, style({ transform: 'translateX(-100%)' }))
+    ]) 
+]);
